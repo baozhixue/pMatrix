0a7465d9c1d1c3c9f7053a9595cc4a9eaffb801a
baozhixue <zhixuebao@outlook.com>
2020-02-08 16:23:15 +0800

    Add files via upload

--- Matrix.h
+++ Matrix.h
@@ -2,8 +2,8 @@
 // created by zhixu on 2020/2/5.
 //
 
-#ifndef lalib_Matrix_h
-#define lalib_Matrix_h
+#ifndef LALIB_MATRIX_H
+#define LALIB_MATRIX_H
 
 #include <iostream>
 #include <immintrin.h>
@@ -13,9 +13,9 @@
 #include <random>
 #include <ctime>
 #include <tuple>
+#include <thread>
 
-
-namespace bzx {
+namespace bzx2 {
 
     constexpr double PI = 3.1415926;
     constexpr double DMIN = std::numeric_limits<double>::min();  // double min
@@ -65,10 +65,11 @@ namespace bzx {
         friend std::tuple<Matrix, Matrix> EigenDec(const Matrix& Mat);
         friend std::tuple<double, Matrix> Power_Method(const Matrix& Mat, const double& min_delta, const size_t& max_iter);
         friend bool fast_copy(Matrix& dst, const Matrix& src);
+        
     public:
         Matrix(const size_t& _row_size, const size_t& _col_size, const double &init_num=0.0);
         Matrix(std::initializer_list<std::initializer_list<double >> src);
-        Matrix() { this->MDesc = new Matrixdsec(0, 0, 1); this->MDesc->memAsyc.push_back(this); };
+        Matrix() = default;// { this->MDesc = new Matrixdsec(0, 0, 1); this->MDesc->memAsyc.push_back(this); };
         Matrix(const Matrix& Mat);
         Matrix& operator=(const Matrix& Mat);
         Matrix& operator=(Matrix* Mat);
@@ -76,9 +77,14 @@ namespace bzx {
         void operator-=(const Matrix& Mat);
         void operator*=(const Matrix& Mat);
         double* operator[](const size_t& index);
+        double* operator[](const size_t& index) const;
+        Matrix operator()(const size_t& Low_r, const size_t& High_r,const size_t& Low_c, const size_t& High_c) const;
+        Matrix operator()(const size_t& r, const size_t& c) const;
         void clear();
         bool memAsycEqual(const Matrix& Mat);
         int use_count()const;
+        bool part_set(const Matrix& Mat, const size_t _Low_r, const size_t& _High_r, const size_t& _Low_c, const size_t& _High_c);
+
 
         Matrix& TRANS();
         std::tuple<size_t, size_t> shape() const {
@@ -95,6 +101,30 @@ namespace bzx {
         Matrixdsec* MDesc = NULL;
 
     };
+
+    
+    class Timer {
+    public:
+        Timer() = default;
+        void SetTimer() { Clock = clock(); Running = true; };
+        double Stop() { 
+            if (Running) {
+                Running = false;
+                return 1.0*(clock() - Clock)/CLOCKS_PER_SEC;
+            }
+            return -1;
+        }
+        double StopAndAgaing() {
+            if (Running) {
+                return 1.0 * (clock() - Clock) / CLOCKS_PER_SEC;
+            }
+            return -1;
+        }
+    private:
+        time_t Clock;
+        bool Running = false;
+    };
+
 
     Matrix::~Matrix() {
         clear();
@@ -118,10 +148,10 @@ namespace bzx {
     void Matrix::clear() {
         //std::cout << "ready clear!\n";
         if (this->MDesc != NULL) {
-            std::cout << "clear all!\n";
+            //std::cout << "waiting clear!\n";
             --MDesc->use_counter;
             if (MDesc->use_counter == 0) {
-                //std::cout << "self clear\n";
+                //std::cout << "clear\n";
                 for (size_t i = 0; i < MDesc->row_size; ++i) {
                     delete[]this->Mat[i];
                     this->Mat[i] = NULL;
@@ -159,9 +189,15 @@ namespace bzx {
             return *this;
         }
 
+        //重新指向新的内存
         if (this->MDesc==NULL || this->MDesc->use_counter <=1) {
+            if (this->shape() == Mat.shape()) {
+                fast_copy(*this, Mat);
+                return *this;
+            }
             clear();
             // 内存为NULL
+            
             size_t _mSize_r, _mSize_c;
             std::tie(_mSize_r, _mSize_c) = Mat.shape();
             this->MDesc = new Matrixdsec(_mSize_r, _mSize_c, 1);
@@ -193,6 +229,7 @@ namespace bzx {
             this->MDesc->memAsyc.push_back(this);
         }
         else {
+            // 更新所有指针所指的地址
             this->memAsycEqual(Mat);
         }
         
@@ -344,7 +381,7 @@ namespace bzx {
                 _mm256_store_pd(dst.Mat[i] + j, _mm256_add_pd(_mm256_load_pd(Mat.Mat[i] + j), _mm256_load_pd(Mat2.Mat[i] + j)));
             }
             while (j < _mSize_c) {
-                dst.Mat[i][j] = Mat.Mat[i][j] - Mat2.Mat[i][j];
+                dst.Mat[i][j] = Mat.Mat[i][j] + Mat2.Mat[i][j];
                 ++j;
             }
         }
@@ -496,10 +533,83 @@ namespace bzx {
         return out;
     }
 
+    //将矩阵的[Low_r,High_r]->[Low_c,High_c]设置为传入矩阵的值,若High_r或High_c为-1则默认为至相应的矩阵最大值
+    bool Matrix::part_set(const Matrix& Mat, const size_t _Low_r, const size_t &_High_r, const size_t &_Low_c, const size_t &_High_c) {
+        size_t High_c, High_r;
+        std::tie(High_r, High_c) = this->shape();
+        if (_High_c < High_c) {
+            High_c = _High_c;
+        }
+        if (_High_r < High_r) {
+            High_r = _High_r;
+        }
+
+        for (size_t i = _Low_r; i < High_r; ++i) {
+            for (size_t j = _Low_c; j < High_c; ++j) {
+                this->Mat[i][j] = Mat[i - _Low_r][j - _Low_c];
+            }
+        }
+
+        return true;
+    }
+
+    // 返回某行或某列
+    /*
+        当使用此函数时，r或c某一个参数必须为-1；
+    */
+    Matrix Matrix::operator()(const size_t& r, const size_t& c) const{
+        assert(r == -1 || c == -1);
+        Matrix child;
+        size_t _mSize_r, _mSize_c;
+        std::tie(_mSize_r, _mSize_c) = this->shape();
+        
+        if (r == -1) {
+            child = Matrix(_mSize_r, 1);
+            for (size_t i = 0; i < _mSize_r; ++i) {
+                child[i][0] = this->Mat[i][c];
+            }
+        }
+        else {
+            child = Matrix(1, _mSize_c);
+            for (size_t i = 0; i < _mSize_c; ++i) {
+                child[0][i] = this->Mat[r][i];
+            }
+        }
+
+        return child;
+    }
 
     double* Matrix::operator[](const size_t& index) {
         return this->Mat[index];
     }
+    double* Matrix::operator[](const size_t& index) const {
+        return this->Mat[index];
+    }
+    /*
+        返回当前矩阵的子矩阵
+        Low_r,Low_c必须小于当前矩阵行数或列数
+    */
+    Matrix Matrix::operator()(const size_t& _Low_r, const size_t& _High_r,
+        const size_t& _Low_c, const size_t& _High_c) const{
+        size_t High_c, High_r;
+        std::tie(High_r, High_c) = this->shape();
+        if (_High_c < High_c) {
+            High_c = _High_c;
+        }
+        if (_High_r < High_r) {
+            High_r = _High_r;
+        }
+        Matrix child(High_r-_Low_r,High_c-_Low_c);
+
+        for (size_t i = _Low_r; i < High_r; ++i) {
+            for (size_t j = _Low_c; j < High_c; ++j) {
+                child[i - _Low_r][j - _Low_c] = this->Mat[i][j];
+            }
+        }
+        return child;// res;
+    }
+
+
 
     /*
      * 转置矩阵自身
@@ -512,7 +622,7 @@ namespace bzx {
     Matrix TRANSPOSITION(const Matrix& Mat) {
         size_t _mSize_r, _mSize_c;
         std::tie(_mSize_r, _mSize_c) = Mat.shape();
-        Matrix res(_mSize_r, _mSize_c);
+        Matrix res(_mSize_c, _mSize_r);
         size_t j = 0;
         for (size_t i = 0; i < _mSize_r; ++i) {
             for (j = 0; j + 4 <= _mSize_c; j += 4) {
@@ -692,18 +802,18 @@ namespace bzx {
             return res;
         }
 
-        Matrix new_subMat(_mSize_r - 1, _mSize_c - 1);
+        Matrix new_subMat(_mSize_r - 1, _mSize_c - 1,0);
         for (size_t j = 0; j < _mSize_c; ++j) {
             for (size_t r = 0; r < _mSize_r; ++r) {
                 for (size_t c = 0; c < _mSize_c; ++c) {
-                    if (_mSize_r != 0 && _mSize_r != _mSize_c) {
+                    if (r != 0 && c != j) {
                         new_subMat.Mat[r > 0 ? r - 1 : r][c > j ? c - 1 : c] = subMat.Mat[r][c];
                     }
                 }
             }
             res += (subMat.Mat[0][j] * (pow(-1, j) * _DETERMINANT(new_subMat)));
         }
-        return res;
+        return abs(res - (1e-5)) > 0 ? res : 0;
     }
 
     /*
@@ -1005,6 +1115,212 @@ namespace bzx {
         return true;
     }
 
+
+    // LU分解
+    std::tuple<Matrix,Matrix> LU(const Matrix& Mat)
+    {
+        size_t _mSize_r, _mSize_c;
+        std::tie(_mSize_r, _mSize_c) = Mat.shape();
+        assert(_mSize_r > 0 && _mSize_r == _mSize_c);
+
+        Matrix L = EYE(_mSize_r);
+        Matrix U(Mat);
+        
+        for (size_t i = 0; i < _mSize_r; ++i) {
+            //U中 i列下方元素变为0；
+            for (size_t j = i + 1; j < _mSize_r; ++j) {
+                L[j][i] = U[j][i] / U[i][i];
+                double ele = U[j][i] / U[i][i];
+                for (size_t k = 0; k < _mSize_c; ++k) {
+                    U[j][k] = U[j][k] - U[i][k] * ele;
+                }
+                
+            }
+        }
+        return std::make_tuple(L,U);
+    }
+
+    std::tuple<Matrix, Matrix> LU2(const Matrix& Mat)
+    {
+        size_t _mSize_r, _mSize_c;
+        std::tie(_mSize_r, _mSize_c) = Mat.shape();
+        assert(_mSize_r > 0 && _mSize_r == _mSize_c);
+
+        Matrix L = EYE(_mSize_r);
+        Matrix U(Mat);
+        __m256d m256D;
+        size_t j, k;
+        double ele;
+        for (size_t i = 0; i < _mSize_r; ++i) {
+            //U中 i列下方元素变为0；
+            for ( j = i + 1; j+2 <= _mSize_r; j +=2) {
+                ele = U[j][i] / U[i][i];
+                L[j][i] = ele;
+                m256D = _mm256_set_pd(ele, ele, ele, ele);
+                
+                for (k = 0; k + 4 <= _mSize_c; k += 4) {
+                    _mm256_store_pd(U[j] + k, _mm256_sub_pd(
+                        _mm256_load_pd(U[j] + k), 
+                        _mm256_mul_pd(_mm256_load_pd(U[i] + k),m256D)
+                    ));
+                }
+                while (k < _mSize_c) {
+                    U[j][k] -= (U[i][k] * ele);
+                    ++k;
+                }
+            }
+            //std::cout << i<<"\n";
+        }
+        return std::make_tuple(L, U);
+    }
+
+    
+
+    void row_swap(Matrix& Mat, size_t i, size_t ii) {
+        size_t _mSize_r, _mSize_c;
+        std::tie(_mSize_r, _mSize_c) = Mat.shape();
+        for (size_t j = 0; j < _mSize_c; ++j) {
+            std::swap(Mat[i][j], Mat[ii][j]);
+        }
+    }
+
+    void row_swap_PLU(Matrix& Mat, size_t i, size_t ii,size_t col_index,bool Left=true) {
+        
+        if (Left) {
+            for (size_t j = 0; j <= col_index; ++j) {
+                std::swap(Mat[i][j], Mat[ii][j]);
+            }
+        }
+        else {
+            size_t _mSize_c;
+            std::tie(std::ignore, _mSize_c) = Mat.shape();
+            for (size_t j = col_index; j <_mSize_c; ++j) {
+                std::swap(Mat[i][j], Mat[ii][j]);
+            }
+        }
+    }
+
+    std::tuple<Matrix, Matrix, Matrix> PLU(const Matrix& Mat)
+    {
+        size_t _mSize_r, _mSize_c;
+        std::tie(_mSize_r, _mSize_c) = Mat.shape();
+       // assert(_mSize_r > 0 && _mSize_r == _mSize_c);
+
+        Matrix P(_mSize_r,_mSize_r);
+        std::vector<size_t> P2(_mSize_r,0);
+        for (size_t i = 0; i < _mSize_r; ++i) {
+            P2[i] = i;
+        }
+        Matrix L = Matrix(_mSize_r, _mSize_r, 0);
+        Matrix U(Mat);
+        size_t max_row_index;
+        size_t current_col = 0;
+        size_t i = 0;
+        __m256d m256D;
+        double ele;
+
+        for (i = 0; i < _mSize_r && current_col <_mSize_c; ++i) {
+            // 寻找该列最大值
+            max_row_index = i;
+
+            for (; current_col < _mSize_c;++current_col) {
+                for (size_t m = i; m < _mSize_r; ++m) {
+                    if (U[max_row_index][current_col] < U[m][current_col]) {
+                        max_row_index = m;
+                    }
+                }
+
+                if (U[max_row_index][current_col] != 0) {
+                    break;
+                }
+            }
+            
+            if (current_col == _mSize_c || max_row_index==_mSize_r) {
+                break;
+            }
+
+            // 交换该轮次最大值行
+            if (max_row_index != i) {
+                std::swap(P2[i], P2[max_row_index]);
+                row_swap_PLU(U, i, max_row_index, current_col, false);
+                row_swap_PLU(L, i, max_row_index, current_col, true);
+            }
+
+            //U中 i列下方元素变为0；
+            for (size_t j = i + 1; j < _mSize_r; ++j) {
+                ele = U[j][current_col] / U[i][current_col];
+                L[j][current_col] = ele;
+                m256D = _mm256_set_pd(ele, ele, ele, ele);
+                size_t k = 0;
+                for (k = current_col; k+4 <= _mSize_c; k+=4) {
+                   // U[j][k] = U[j][k] - U[i][k] * ele;
+                    _mm256_store_pd(U[j] + k, _mm256_sub_pd(
+                        _mm256_load_pd(U[j] + k), 
+                        _mm256_mul_pd(_mm256_load_pd(U[i] + k),m256D)
+                    ));
+                }
+                while (k < _mSize_c) {
+                    U[j][k] -= (U[i][k] * ele);
+                    ++k;
+                
+                }
+
+            }
+            current_col += 1;
+        }
+        
+        for (size_t i = 0; i < _mSize_r; ++i) {
+            P[i][P2[i]] = 1;
+        }
+
+        L += (EYE(_mSize_r));
+        return std::make_tuple(P, L, U);
+    }
+
+
+    // 2范数,[1,n]
+    double Norm_2(const Matrix& Mat) {
+        double res = 0;
+        Matrix ATA = TRANSPOSITION(Mat) * Mat;
+        std::tie(res, std::ignore) = Power_Method(ATA);
+        return sqrt(res);
+    }
+
+   
+    /*
+        格里姆施密特正交法
+    */
+    std::tuple<Matrix,Matrix> QR(const Matrix& Mat) {
+        
+        size_t _mSize_r, _mSize_c;
+        std::tie(_mSize_r, _mSize_c) = Mat.shape();
+
+        Matrix Q(_mSize_r, _mSize_c);
+        Matrix R(_mSize_c, _mSize_c);
+        Matrix q(_mSize_r, 1);
+        Matrix y(_mSize_r, 1);
+        Matrix yT;
+        size_t j, i;
+        double norm_2 = 0;
+        for (j = 0; j < _mSize_c; ++j) {
+            // y = A[][j]
+            y = Mat(-1, j);
+            // q(i)
+            
+            for (i = 0; j>0 && i < j; ++i) {
+                q = Q(-1, i);
+                yT = &(TRANSPOSITION(q) * y);
+                R[i][j] = yT[0][0];
+                y -= DOT(q, R[i][j]); //y -= (q * R[i][j]);
+            }
+            norm_2 = Norm_2(y);
+            R[j][j] = norm_2;
+            q = y / norm_2;
+            Q.part_set(q, 0, -1, j, j + 1);
+        }
+        return std::make_tuple(Q, R);
+    }
+    
 }
 
-#endif //lalib_Matrix_h
+#endif //LALIB_MATRIX_H
